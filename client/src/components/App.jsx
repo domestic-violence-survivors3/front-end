b86cc81574399ede6bb143146fe237c9ccd858f9
Robert J. Mitchell <robert.judson@outlook.com>
2020-02-03 11:56:49 -0500

    Login and SignUp pages started, Private Route started

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
+import { Route, Switch, Link } from "react-router-dom";
 
-import '../css/App.css';
+import "../css/App.css";
+import PrivateRoute from "./PrivateRoute"
+import SignUp from "./SignUp"; 
+import Login from "./Login";
+import Calculator from "./Calculator";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <h1>Domestic Violence Survivors</h1>
+
+      <div>
+        <Link to="/signup">Sign Up</Link>
+      </div>
+      <div>
+        <Link to="/login">Login</Link>
+      </div>
+      <div>
+        <Link to="/calculator">Calculator</Link>
+      </div>
+
+      <Switch>
+        <PrivateRoute path="/calculator" component={Calculator} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/login" component={Login} />
+        <Route component={Login} />
+      </Switch>
     </div>
   );
 }
