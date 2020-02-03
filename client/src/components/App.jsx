e592365d59fabf1f7dcc85c48f934750b6f5d215
Robert J. Mitchell <robert.judson@outlook.com>
2020-02-03 11:08:42 -0500

    update to file structure, file naming convention initiated

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Domestic Violence Survivors</h1>
+    </div>
+  );
+}
+
+export default App;
