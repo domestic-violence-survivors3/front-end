20460012e6fa3399c13a902cba3196b176369917
unknown <ksemenza13@gmail.com>
2020-02-04 14:50:28 -0500

    update file strucutre and style

--- client/src/components/PersonalForm.jsx
+++ client/src/components/PersonalForm.jsx
@@ -1,119 +1,77 @@
 import React, { useState, useEffect } from 'react';
-import {withFormik, Form, Field } from 'formik'
-import {Link, useRouteMatch} from 'react-router-dom'
+import { withFormik, Form, Field } from 'formik'
 import axios from 'axios'
 import * as Yup from 'yup';
-import styled from 'styled-components'
-import PersonalList from './PersonalExpense'
-
-const TitleHeader = styled.h1`
-
-margin-bottom: -15px;
-width 33vw;
-margin 0 auto;
-padding: 15px 0;
-
-`
-
-const FormWarp = styled.div`
-    width: 33vw;
-    display:flex;
-    flex-direction:column;
-    padding: 20px 20px;
-    margin: 15px auto;
-    display: flex;
-    flex-flow: column nowrap;
-    justify-content: stretch;
-    align-items: stretch;
-`;
-const FieldCta = styled.div`
-    display: flex;
-    flex-direction:column;
-    padding-bottom: 45px;
-    padding-left: 155px;
-    padding-right: 155px;
-`
-const ErrorPrompt = styled.p`
-    color:#8AC748;
-    margin-bottom:-20px;
-    margin-top:-2px;
-    font-weight: 600;
-`
-
-const BorderWrap = styled.div`
-width: 33vw;
-margin: 0 auto;
-padding-bottom: 32px;
-`
+import { BorderWrap, TitleHeader, FormWarp, FieldCta, ErrorPrompt } from '../assests/Styles'
+import PersonalTotal from './PersonalTotal'
 
 const PersonalForm = ({ values, errors, touched, status }, props) => {
-    const [personalExpenses, setPersonalExpenses] = useState([]);
-
-
+    const [expenses, setExpenses] = useState([]);
 
     useEffect(() => {
-        status && setPersonalExpenses(personalExpenses => [...personalExpenses, status]);
-        
+        status && setExpenses(personalExpenses => [...personalExpenses, status]);
     }, [status]);
     return (
-      
         <div>
             <BorderWrap>
-                <TitleHeader>Personal Expenses</TitleHeader>
+                <h2>Personal Expenses</h2>
                 <Form>
                     <FormWarp>
                         <FieldCta>
-                            <label htmlFor='transportation'>Transportation</label>
-                            <Field id='transportation' value={values.transportation} type='number' name='transportation' style={{ padding: `5px` }} />
-                            {touched.transportation && errors.transportation && (<ErrorPrompt>{errors.transportation}</ErrorPrompt>)}
+                            <label htmlFor='transportation' style={{ color: `black` }}>
+                                <Field id='transportation' value={values.transportation} type='number' name='transportation' placeholder='Transporation' style={{ padding: `5px` }} />
+                                {touched.transportation && errors.transportation && (<ErrorPrompt>{errors.transportation}</ErrorPrompt>)}
+                            </label>
                         </FieldCta>
                         <FieldCta>
-                            <label htmlFor='food'>Food</label>
-                            <Field id='food'value={values.food} type='number' name='food'style={{ padding: `5px` }} />
-                            {/* {touched.food && errors.food && (<ErrorPrompt>{errors.food}</ErrorPrompt>)} */}
+                            <label htmlFor='food'>
+                                <Field id='food' value={values.food} type='number' name='food' placeholder='Food' style={{ padding: `5px` }} />
+                                {/* {touched.food && errors.food && (<ErrorPrompt>{errors.food}</ErrorPrompt>)} */}
+                            </label>
                         </FieldCta>
                         <FieldCta>
-                            <label htmlFor='healthInsurance'>Health Insurance</label>
-                            <Field id='healthInsurance' value={values.healthInsurance} type='number' name='healthInsurance'style={{ padding: `5px` }} />
-                            {touched.healthInsurance && errors.healthInsurance && (<ErrorPrompt>{errors.healthInsurance}</ErrorPrompt>)}
+                            <label htmlFor='healthInsurance'>
+                                <Field id='healthInsurance' value={values.healthInsurance} type='number' name='healthInsurance' placeholder='Health Insurance' style={{ padding: `5px` }} />
+                                {touched.healthInsurance && errors.healthInsurance && (<ErrorPrompt>{errors.healthInsurance}</ErrorPrompt>)}
+                            </label>
                         </FieldCta>
                         <FieldCta>
-                            <label htmlFor='carInsurance'>Car Insurance</label>
-                            <Field id='carInsurance'  value={values.carInsurance} type='number' name='carInsurance'style={{ padding: `5px` }} />
-                            {touched.carInsurance && errors.carInsurance && (<ErrorPrompt>{errors.carInsurance}</ErrorPrompt>)}
+                            <label htmlFor='carInsurance'>
+                                <Field id='carInsurance' value={values.carInsurance} type='number' name='carInsurance' placeholder='Car Insurance' style={{ padding: `5px` }} />
+                                {touched.carInsurance && errors.carInsurance && (<ErrorPrompt>{errors.carInsurance}</ErrorPrompt>)}
+                            </label>
                         </FieldCta>
                         <FieldCta>
-                            <label htmlFor='loans'>Loans</label>
-                            <Field id='loans' value={values.loans} type='number' name='loans'style={{ padding: `5px` }} />
-                            {touched.loans && errors.loans && (<ErrorPrompt>{errors.loans}</ErrorPrompt>)}
+                            <label htmlFor='loans'>
+                                <Field id='loans' value={values.loans} type='number' name='loans' placeholder='Loans' style={{ padding: `5px` }} />
+                                {touched.loans && errors.loans && (<ErrorPrompt>{errors.loans}</ErrorPrompt>)}
+                            </label>
                         </FieldCta>
                         <FieldCta>
-                            <label htmlFor='otherExpense'>Other Expenses</label>
-                            <Field id='otherExpense' value={values.otherExpense} type='number' name='otherExpense'style={{ padding: `5px` }} />
-                            {touched.otherExpense && errors.otherExpense && (<ErrorPrompt>{errors.otherExpense}</ErrorPrompt>)}
+                            <label htmlFor='otherExpense'>
+                                <Field id='otherExpense' value={values.otherExpense} type='number' name='otherExpense' placeholder='Other Expenses' style={{ padding: `5px` }} />
+                                {touched.otherExpense && errors.otherExpense && (<ErrorPrompt>{errors.otherExpense}</ErrorPrompt>)}
+                            </label>
                         </FieldCta>
-{/* FIELD TEMPLATE
+                        {/* FIELD TEMPLATE
                         <FieldCta>
                             <label htmlFor=''></label>
                             <Field type='text' name='' placeholder='' style={{ padding: `5px` }} />
                             {touched. && errors. && (<ErrorPrompt>{errors.}</ErrorPrompt>)}
                         </FieldCta> */}
-
-           
                     </FormWarp>
-                    <PersonalList personalExpenses={personalExpenses} />
+                    <button type='submit' >Next</button>
 
-                    <Link to='/move-expenses'>
-                    <button type='submit' renderAs='button'>Next</button>
-                    </Link>
                 </Form>
             </BorderWrap>
+            <PersonalTotal personalExpenses={expenses} />
+
         </div>
     )
 };
 
-const FormikPersonalForm= withFormik({
-    mapPropsToValues({ transportation, food, healthInsurance,carInsurance,loans, otherExpense }) {
+const FormikPersonalForm = withFormik({
+    mapPropsToValues({ transportation, food, healthInsurance, carInsurance, loans, otherExpense }) {
         return {
             transportation: transportation || '',
             food: food || '',
@@ -132,20 +90,15 @@ const FormikPersonalForm= withFormik({
 
     }),
 
-
-
-    handleSubmit(values, { setStatus, resetForm }) {
-        axios.post('https://reqres.in/api/users', values)
+    handleSubmit(expenses, { setStatus, resetForm }) {
+        axios.post('https://reqres.in/api/users', expenses)
             .then(res => {
                 // console.log(res.data);
                 setStatus(res.data);
                 resetForm();
-                
             })
-
-       
             .catch(err => console.log(err.response));
     }
-})(PersonalForm);
+}, [])(PersonalForm);
 
 export default FormikPersonalForm;
