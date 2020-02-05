d2255968eeea26e6e6c854778c2ddc6faf575b56
unknown <ksemenza13@gmail.com>
2020-02-05 04:15:00 -0500

    Merged
    Signed-off-by: unknown <ksemenza13@gmail.com>

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
-<<<<<<< HEAD
-=======
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-
-import ExpenseHeader from './ExpenseHeader';
->>>>>>> d8dda15b181dc6cb825177cc2539d29f3407774e
 import PersonalBudget from './PersonalBudgets'
-
 const Dashboard = props => {
+// console.log(props.firstname)
   return(
-<<<<<<< HEAD
     <div className='dashboard-cta'>
     <h2>Welcome to Your Dashboard!</h2>
   <div className='dashboard-header'>
@@ -20,19 +13,11 @@ const Dashboard = props => {
   <h3>{props.id}</h3>
   
     </div>
-=======
-    <div className="dashboard">
-      <div className='dashboard-cta'>
-        <h2>Welcome to Your Dashboard!</h2>
-        <div className='dashboard-header'>
-          <h5 className='welcome-user'>Hello User</h5> 
-          <h5 className='last-budget'>Most Recent Budget<span></span></h5>
-        </div>  
-        <PersonalBudget />
-        <h3>{props.id}</h3>
-      </div>
->>>>>>> d8dda15b181dc6cb825177cc2539d29f3407774e
 
+    
+
+
+   
   )
 }
 
